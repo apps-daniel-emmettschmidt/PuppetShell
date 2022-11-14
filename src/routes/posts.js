8ec2f1e244db0a6e25b8a0fa889702f71fc34e24
apps-daniel-emmettschmidt <apps-daniel@emmettschmidt.com>
2022-11-13 22:43:59 -0500

    Basic page navigation

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -2,10 +2,55 @@
 const express = require('express')
 const reader = express.Router()
 
+const puppeteer = require('puppeteer');
+
+async function startBrowser(address, waitFor){
+	let result = await setGetPage(address, waitFor);
+	
+	return result;
+}
+
+async function setGetPage(address, waitFor)
+{
+  try {
+    console.log("Opening the browser......");
+    const browser = await puppeteer.launch({
+        headless: false,
+        args: ["--disable-setuid-sandbox"],
+        'ignoreHTTPSErrors': true
+    });
+
+    const page = await browser.newPage();
+
+    page.setDefaultNavigationTimeout(240000);
+
+    await page.goto(address);
+
+    if(waitFor === undefined)
+    {
+      await page.waitForNavigation();
+    }
+    else
+    {
+      await page.waitForSelector(waitFor);
+    }
+
+    console.log("Done navigating to " + address);
+
+
+  } catch (err) {
+      console.log("Could not create a browser instance => : ", err);
+  }
+
+}
 
 reader.get('/', (req, res, next) => {
 
   const address = req.query.address;
+
+  const waitFor = req.query.waitFor;
+
+  startBrowser(address, waitFor);
 
   res.send('Heard ' + address);
     // 'You have hit GET /posts endpoint')
